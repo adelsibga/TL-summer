5d4ad38fdfdf8818f0f49964f1c24cc491fe2852
Adel Sibgatullin <adelsibga@gmail.com>
2023-07-04 22:52:19 +0300

    add bubble sort

---
+++ homeTask/bubbleSort/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = {800, 11, 50, 771, 649, 770, 240, 9};
+
+int tmp = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    for (int j = 0; j < arr.Length - 1; j++)
+    {
+        if (arr[j] > arr[j + 1])
+        {
+            tmp = arr[j + 1];
+            arr[j + 1] = arr[j];
+            arr[j] = tmp;
+        }
+    }
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i] + " ");
+}
