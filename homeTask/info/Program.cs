8c4ff9554eed35751aada0fb21adc48ce45f09c8
Adel Sibgatullin <adelsibga@gmail.com>
2023-07-04 23:15:44 +0300

    add info task

---
+++ homeTask/info/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter your full name");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter your age");
+string age = Console.ReadLine();
+
+Console.WriteLine("Enter your email");
+string email = Console.ReadLine();
+
+Console.WriteLine("Enter your github link");
+string github = Console.ReadLine();
+
+Console.Clear();
+Console.Write(
+    $"Full name: {name},\n" +
+    $"Age: {age},\n" +
+    $"Email: {email},\n" +
+    $"Github link: {github}"
+);
