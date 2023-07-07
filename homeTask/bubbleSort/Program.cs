67b6c436d97c8ae0a7dabda328b8eb32cfb017fe
Adel Sibgatullin <adelsibga@gmail.com>
2023-07-07 10:03:55 +0300

    Fix bubbleSort issues

--- homeTask/bubbleSort/Program.cs
+++ homeTask/bubbleSort/Program.cs
@@ -1,38 +1,32 @@
 ﻿int[] elements = { 800, 11, 50, 771, 649, 770, 240, 9 };
-
 int length = elements.Length;
 
-bool isSorted(int[] arr)
+void bubbleSort(int[] arr)
 {
-    int j = arr.Length - 1;
-    int i = 1;
-    int element = arr[0];
+    bool swapped;
 
-    if (j < 1)
+    for(int i = 0; i < length; i++)
     {
-        return true;
-    }
+        swapped = false;
 
-    while (i <= j && arr[0] <= (element = arr[i])) i++;
-    {
-        return i > j;
-    }
-}
-
-if (!isSorted(elements))
-{
-    for (int i = 0; i < length; i++)
-    {
-        for (int j = 0; j < length - 1 - i; j++)
+        for(int j = 0; j < length - 1 - i; j++)
         {
-            if (elements[j] > elements[j + 1])
+            if(elements[j] > elements[j + 1])
             {
+                swapped = true;
+
                 elements[j] = elements[j] + elements[j + 1];
                 elements[j + 1] = elements[j] - elements[j + 1];
                 elements[j] = elements[j] - elements[j + 1];
             }
         }
+
+        if(!swapped)
+        {
+            break;
+        }
     }
 }
 
+bubbleSort(elements);
 Console.WriteLine(String.Join(" ", elements));
