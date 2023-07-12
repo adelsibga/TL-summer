1ea57e5e8b9077c864e74836bc53287c9d6d0af8
Adel Sibgatullin <adelsibga@gmail.com>
2023-07-12 12:14:39 +0300

    Add shapes

---
+++ homeTask/Shapes/Square.cs
@@ -0,0 +1,28 @@
+﻿namespace Shapes;
+
+public class Square : IShape
+{
+    private double _side;
+    public double sideLength
+    {
+        get => _side;
+        set
+        {
+            if(value <= 0)
+            {
+                throw new ArgumentException("Side must be positive");
+            }
+            _side = value;
+        }
+    }
+
+    public double CalculateArea()
+    {
+        return sideLength * sideLength;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return 4 * sideLength;
+    }
+}
